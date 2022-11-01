12f9d3d1114ae4a4ffd68d717c3a46a323a365ea
Howard Tai <howard@howardism.dev>
2022-11-01 21:17:11 +0800

    feat: set up Card in core with test

--- packages/jest-config/esbuild.js
+++ packages/jest-config/esbuild.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 /** @type {import('jest').Config} */
 const config = {
   ...require('./common'),
+  setupFilesAfterEnv: [path.join(__dirname, 'esbuild-jest.setup.js')],
   transform: {
     '^.+\\.jsx?$': 'esbuild-jest',
     '^.+\\.tsx?$': 'esbuild-jest',
