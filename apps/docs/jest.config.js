5fde50a138e486139bd485fe28df548eb4dd7e61
Howard Tai <howard@howardism.dev>
2022-12-11 20:11:08 +0800

    test: revert changes of apps/docs jest config

--- apps/docs/jest.config.js
+++ apps/docs/jest.config.js
@@ -1,6 +1,1 @@
-const config = require('@org/jest-config/common')
-const nextJest = require('next/jest')
-
-const createJestConfig = nextJest({ dir: './' })
-
-module.exports = createJestConfig({ ...config, setupFilesAfterEnv: ['./jest.setup.js'] })
+module.exports = require('@org/jest-config/next')
